2ed2d5fa03f763ab4947767ad24cd61fa1936cc0
Michael Chapman <michael.chapman@anchor.net.au>
2010-04-19 07:26:56 +0000

    Yes, it's a sudo module. We're always doing "augeas_for_sudo_*" everywhere, and it'd be a lot saner if this was properly Puppet-typed. For now, this module just incorporates the legacy sudo classes and disables the requiretty option so that NRPE checks are happy (I'm always doing this manually when building servers...). The requiretty is left behind commented, so it's clear what Red Hat's comment block above it is talking about.

---
+++ manifests/legacy.pp
@@ -0,0 +1,14 @@
+class sudo::build inherits sudo::base {
+}
+
+class sudo::monitor {
+}
+
+class sudo::secure inherits sudo::base {
+}
+
+class sudo::complete inherits sudo::secure {
+}
+
+class sudo::engineroom inherits sudo::complete {
+}
