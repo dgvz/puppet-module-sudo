2d58c99e93bf04ed7285a80d6ded6bbae6c94a86
Michael Chapman <michael.chapman@anchor.net.au>
2010-04-22 00:33:06 +0000

    - Make "passwd => false" actually work - Never add a Sudo entry for "root"

--- manifests/entry.pp
+++ manifests/entry.pp
@@ -1,9 +1,10 @@
 define sudo::entry::tag_($base, $tagname, $value) {
-	if $value {
-		case $value {
-			yes,true,1: { $tag_ = $tagname       }
-			no,false,0: { $tag_ = "NO${tagname}" }
-		}
+	case $value {
+		yes,true,1: { $tag_ = $tagname       }
+		no,false,0: { $tag_ = "NO${tagname}" }
+		default:    { $tag_ = undef          }
+	}
+	if $tag_ {
 		augeas { $name:
 			context => "/files/etc/sudoers",
 			changes => "set ${base}/tag[.='${tagname}' or .='NO${tagname}'] ${tag_}",
@@ -28,31 +29,35 @@ define sudo::entry($ensure   = present,
                    $setenv   = undef) {
 	include sudo
 
-	$base = "spec[user='${user}']/host_group[host='${host}']/command[.='${command}'][runas_user='${runas}']"
+	if $user == "root" {
+		warning("Refusing to add Sudo entry for '${user}'")
+	} else {
+		$base = "spec[user='${user}']/host_group[host='${host}']/command[.='${command}'][runas_user='${runas}']"
 
-	case $ensure {
-		present: {
-			augeas { $name:
-				context => "/files/etc/sudoers",
-				changes => [
-					"set spec[last()+1]/user ${user}",
-					"set spec[last()]/host_group/host ${host}",
-					"set spec[last()]/host_group/command '${command}'",
-					"set spec[last()]/host_group/command/runas_user ${runas}",
-				],
-				onlyif  => "match ${base} size == 0";
-			}
+		case $ensure {
+			present: {
+				augeas { $name:
+					context => "/files/etc/sudoers",
+					changes => [
+						"set spec[last()+1]/user ${user}",
+						"set spec[last()]/host_group/host ${host}",
+						"set spec[last()]/host_group/command '${command}'",
+						"set spec[last()]/host_group/command/runas_user ${runas}",
+					],
+					onlyif  => "match ${base} size == 0";
+				}
 
-			Sudo::Entry::Tag_ {
-				base    => $base,
-				require => Augeas[$name],
+				Sudo::Entry::Tag_ {
+					base    => $base,
+					require => Augeas[$name],
+				}
+				sudo::entry::tag_ { "${name}/passwd": tagname => "PASSWD", value => $passwd }
+				sudo::entry::tag_ { "${name}/exec":   tagname => "EXEC",   value => $exec   }
+				sudo::entry::tag_ { "${name}/setenv": tagname => "SETENV", value => $setenv }
 			}
-			sudo::entry::tag_ { "${name}/passwd": tagname => "PASSWD", value => $passwd }
-			sudo::entry::tag_ { "${name}/exec":   tagname => "EXEC",   value => $exec   }
-			sudo::entry::tag_ { "${name}/setenv": tagname => "SETENV", value => $setenv }
-		}
-		absent: {
-			fail("ensure '${ensure}' not implemented")
+			absent: {
+				fail("ensure '${ensure}' not implemented")
+			}
 		}
 	}
 }
