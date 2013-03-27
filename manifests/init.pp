6e108e7943d95846e20456eebf0baf0489ad5c5e
Steven McDonald <steven.mcdonald@anchor.net.au>
2013-03-28 08:59:14 +1100

    Red Hat's secure_path is broken

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,4 +13,21 @@ class sudo {
 		],
 		onlyif => "match Defaults/requiretty size > 0";
 	}
+
+	case $::operatingsystem {
+		RedHat,CentOS: {
+			if to_i($::operatingsystemrelease) >= 6 {
+				# Red Hat doesn't like /usr/local, apparently. Let's fix
+				# that by making secure_path consistent with Debian's.
+				augeas { "sudo/secure_path":
+					incl    => "/etc/sudoers",
+					lens    => "Sudoers.lns",
+					changes => [
+						"set Defaults/secure_path \"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"",
+					],
+					onlyif  => "match Defaults/secure_path \"/sbin:/bin:/usr/sbin:/usr/bin\"",
+				}
+			}
+		}
+	}
 }
